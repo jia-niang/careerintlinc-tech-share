a0b2d809494c1d039145b4ec25ad602f8fdffdff
jia-niang <1@paperplane.cc>
2024-04-10 22:25:47 +0800

    chore(cdn): 现在会尝试将资源上传 CDN，修改 publicPath

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'dumi'
+// @ts-ignore
+import S3Plugin from 'webpack-s3-plugin'
 
 export default defineConfig({
   themeConfig: {
@@ -6,5 +8,25 @@ export default defineConfig({
     logo: '/logo.svg',
     favicons: ['/favicon.ico'],
     footer: false,
+  },
+
+  chainWebpack(memo: any) {
+    if (process.env.NODE_ENV === 'production') {
+      memo.output.publicPath('https://cdn.paperplane.cc/paperplane-share/')
+      memo.plugin('S3Plugin').use(S3Plugin, {
+        exclude: /.*\.html$/,
+        basePath: 'paperplane-share',
+        s3Options: {
+          accessKeyId: process.env.COS_SECRET_ID,
+          secretAccessKey: process.env.COS_SECRET_KEY,
+          region: 'ap-hongkong',
+          endpoint: 'https://cos.ap-hongkong.myqcloud.com',
+          apiVersion: '2006-03-01',
+        },
+        s3UploadOptions: {
+          Bucket: 'paperplane-cdn-1253277322',
+        },
+      })
+    }
   },
 })
