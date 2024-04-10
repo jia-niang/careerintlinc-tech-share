c6a3d57806556f62d73180aefdf82ddc9f11e227
jia-niang <1@paperplane.cc>
2024-04-10 22:32:54 +0800

    fix(s3-plugin): 现在以正确的传参方式启用 S3 插件

--- .dumirc.ts
+++ .dumirc.ts
@@ -13,20 +13,22 @@ export default defineConfig({
   chainWebpack(memo: any) {
     if (process.env.NODE_ENV === 'production') {
       memo.output.publicPath('https://cdn.paperplane.cc/paperplane-share/')
-      memo.plugin('S3Plugin').use(S3Plugin, {
-        exclude: /.*\.html$/,
-        basePath: 'paperplane-share',
-        s3Options: {
-          accessKeyId: process.env.COS_SECRET_ID,
-          secretAccessKey: process.env.COS_SECRET_KEY,
-          region: 'ap-hongkong',
-          endpoint: 'https://cos.ap-hongkong.myqcloud.com',
-          apiVersion: '2006-03-01',
+      memo.plugin('S3Plugin').use(S3Plugin, [
+        {
+          exclude: /.*\.html$/,
+          basePath: 'paperplane-share',
+          s3Options: {
+            accessKeyId: process.env.COS_SECRET_ID,
+            secretAccessKey: process.env.COS_SECRET_KEY,
+            region: 'ap-hongkong',
+            endpoint: 'https://cos.ap-hongkong.myqcloud.com',
+            apiVersion: '2006-03-01',
+          },
+          s3UploadOptions: {
+            Bucket: 'paperplane-cdn-1253277322',
+          },
         },
-        s3UploadOptions: {
-          Bucket: 'paperplane-cdn-1253277322',
-        },
-      })
+      ])
     }
   },
 })
